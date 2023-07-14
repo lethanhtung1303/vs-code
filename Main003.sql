29ad11bb166adc5acdccdc18c014bb42c51ca3a6
lethanhtung1303 <letung844@gmail.com>
2023-07-14 23:46:02 +0700

    add ERD

--- Main003.sql
+++ Main003.sql
@@ -1,44 +1,47 @@
--- Tạo bảng Khách hàng
-CREATE TABLE Customer (
-    CustomerID INT PRIMARY KEY,
-    Name VARCHAR(255),
-    Address VARCHAR(255),
+-- Tạo bảng Người dùng (User)
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY,
+    Username VARCHAR(255),
+    Email VARCHAR(255),
+    Password VARCHAR(255),
     PhoneNumber VARCHAR(20)
 );
 
--- Tạo bảng Sản phẩm
-CREATE TABLE Product (
+-- Tạo bảng Sản phẩm (Product)
+CREATE TABLE Products (
     ProductID INT PRIMARY KEY,
     ProductName VARCHAR(255),
+    ProductDescription TEXT,
     Price DECIMAL(10, 2),
-    Description TEXT
+    Quantity INT
 );
 
--- Tạo bảng Đơn hàng
+-- Tạo bảng Đơn hàng (Order)
 CREATE TABLE Orders (
     OrderID INT PRIMARY KEY,
-    CustomerID INT,
+    UserID INT,
     OrderDate DATE,
     OrderStatus VARCHAR(50),
-    TotalPrice DECIMAL(10, 2),
-    FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
+    DeliveryStaffID INT,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (DeliveryStaffID) REFERENCES DeliveryStaff(StaffID)
 );
 
--- Tạo bảng Nhân viên giao hàng
-CREATE TABLE DeliveryStaff (
-    StaffID INT PRIMARY KEY,
-    StaffName VARCHAR(255),
-    CurrentLocation VARCHAR(255),
-    WorkingStatus VARCHAR(50)
-);
-
--- Tạo bảng Chi tiết đơn hàng
-CREATE TABLE OrderDetail (
+-- Tạo bảng Chi tiết đơn hàng (OrderDetail)
+CREATE TABLE OrderDetails (
     OrderDetailID INT PRIMARY KEY,
     OrderID INT,
     ProductID INT,
     Quantity INT,
-    Subtotal DECIMAL(10, 2),
+    Price DECIMAL(10, 2),
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
-    FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
+
+-- Tạo bảng Nhân viên giao hàng (DeliveryStaff)
+CREATE TABLE DeliveryStaff (
+    StaffID INT PRIMARY KEY,
+    StaffName VARCHAR(255),
+    Email VARCHAR(255),
+    PhoneNumber VARCHAR(20)
 );
